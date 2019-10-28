04fbecb9281a172119f7533d5e58180453819331
Vinoaj Vijeyakumaar <vinoaj@gmail.com>
2019-10-28 22:31:42 +1100

    cleanup

--- export_GA360_realtime_view.sql
+++ export_GA360_realtime_view.sql
@@ -1,3 +1,10 @@
+#standardSQL
+#find the highest index custom dimension
+SELECT MAX((SELECT MAX(index) FROM UNNEST(hits), UNNEST(customDimensions))) AS max_cd_ix
+FROM `XXXXXX.158895650.ga_sessions_20180912` 
+LIMIT 100
+
+
 #legacySQL
 SELECT visitId
   , visitNumber
@@ -8,7 +15,14 @@ SELECT visitId
   , trafficSource.*
   , device.*
   , geoNetwork.*
+  , hits.time
+  , hits.isSecure
+  , hits.isInteraction
+  , hits.isEntrance
+  , hits.isExit
+  , hits.referer
   , hits.type
+  , hits.dataSource
   , hits.page.*
   , hits.transaction.*
   , hits.item.*
@@ -24,7 +38,36 @@ SELECT visitId
   , hits.latencyTracking.*
   , hits.sourcePropertyInfo.*
   , hits.contentGroup.*
-  , hits.dataSource
+  , MAX(IF(hits.customDimensions.index=1, hits.customDimensions.value, NULL)) WITHIN hits AS cd01
+  , MAX(IF(hits.customDimensions.index=2, hits.customDimensions.value, NULL)) WITHIN hits AS cd02
+  , MAX(IF(hits.customDimensions.index=3, hits.customDimensions.value, NULL)) WITHIN hits AS cd03
+  , MAX(IF(hits.customDimensions.index=4, hits.customDimensions.value, NULL)) WITHIN hits AS cd04
+  , MAX(IF(hits.customDimensions.index=5, hits.customDimensions.value, NULL)) WITHIN hits AS cd05
+  , MAX(IF(hits.customDimensions.index=6, hits.customDimensions.value, NULL)) WITHIN hits AS cd06
+  , MAX(IF(hits.customDimensions.index=7, hits.customDimensions.value, NULL)) WITHIN hits AS cd07
+  , MAX(IF(hits.customDimensions.index=8, hits.customDimensions.value, NULL)) WITHIN hits AS cd08
+  , MAX(IF(hits.customDimensions.index=9, hits.customDimensions.value, NULL)) WITHIN hits AS cd09
+  , MAX(IF(hits.customDimensions.index=10, hits.customDimensions.value, NULL)) WITHIN hits AS cd10
+  , MAX(IF(hits.customDimensions.index=11, hits.customDimensions.value, NULL)) WITHIN hits AS cd11
+  , MAX(IF(hits.customDimensions.index=12, hits.customDimensions.value, NULL)) WITHIN hits AS cd12
+  , MAX(IF(hits.customDimensions.index=13, hits.customDimensions.value, NULL)) WITHIN hits AS cd13
+  , MAX(IF(hits.customDimensions.index=14, hits.customDimensions.value, NULL)) WITHIN hits AS cd14
+  , MAX(IF(hits.customDimensions.index=15, hits.customDimensions.value, NULL)) WITHIN hits AS cd15
+  , MAX(IF(hits.customDimensions.index=16, hits.customDimensions.value, NULL)) WITHIN hits AS cd16
+  , MAX(IF(hits.customDimensions.index=17, hits.customDimensions.value, NULL)) WITHIN hits AS cd17
+  , MAX(IF(hits.customDimensions.index=18, hits.customDimensions.value, NULL)) WITHIN hits AS cd18
+  , MAX(IF(hits.customDimensions.index=19, hits.customDimensions.value, NULL)) WITHIN hits AS cd19
+  , MAX(IF(hits.customDimensions.index=20, hits.customDimensions.value, NULL)) WITHIN hits AS cd20
+  , MAX(IF(hits.customDimensions.index=21, hits.customDimensions.value, NULL)) WITHIN hits AS cd21
+  , MAX(IF(hits.customDimensions.index=22, hits.customDimensions.value, NULL)) WITHIN hits AS cd22
+  , MAX(IF(hits.customDimensions.index=23, hits.customDimensions.value, NULL)) WITHIN hits AS cd23
+  , MAX(IF(hits.customDimensions.index=24, hits.customDimensions.value, NULL)) WITHIN hits AS cd24
+  , MAX(IF(hits.customDimensions.index=25, hits.customDimensions.value, NULL)) WITHIN hits AS cd25
+  , MAX(IF(hits.customDimensions.index=26, hits.customDimensions.value, NULL)) WITHIN hits AS cd26
+  , MAX(IF(hits.customDimensions.index=27, hits.customDimensions.value, NULL)) WITHIN hits AS cd27
+  , MAX(IF(hits.customDimensions.index=28, hits.customDimensions.value, NULL)) WITHIN hits AS cd28
+  , MAX(IF(hits.customDimensions.index=29, hits.customDimensions.value, NULL)) WITHIN hits AS cd29
+  , MAX(IF(hits.customDimensions.index=30, hits.customDimensions.value, NULL)) WITHIN hits AS cd30
   , fullVisitorId
   , userId
   , clientId
@@ -33,5 +76,27 @@ SELECT visitId
   , exportTimeUsec
   , exportKey
   , visitKey
-FROM [XXXXXX.ga_realtime_sessions_view_20180912] 
+FROM [XXXXXX.ga_realtime_sessions_view_20180914] 
 ORDER BY visitId, hits.hitNumber ASC
+
+
+#standardSQL
+#count number of instances of custom dimensions
+WITH nested_cd_data AS (
+  SELECT visitId
+  , hitNumber
+  , (SELECT ARRAY_AGG(index) FROM UNNEST(hits), UNNEST(customDimensions)) AS cd_ixs
+  FROM `XXXXXX.XXXXXX.ga_sessions_20180912`, UNNEST(hits)
+)
+
+SELECT cd_ix, COUNT(cd_ix) AS count
+FROM nested_cd_data, UNNEST(cd_ixs) AS cd_ix
+GROUP BY cd_ix
+ORDER BY cd_ix ASC
+
+
+#standardSQL
+#find the highest index custom dimension
+SELECT MAX((SELECT MAX(index) FROM UNNEST(hits), UNNEST(customDimensions))) AS max_cd_ix
+FROM `XXXXXX.XXXXXX.ga_sessions_20180912` 
+LIMIT 100
--- firebase/list-event-parameters.sql
+++ firebase/list-event-parameters.sql
@@ -20,3 +20,27 @@ ORDER BY
   , eventAction ASC
   -- , eventValue ASC
 LIMIT 1000
+
+
+
+#standardSQL
+WITH event_params_unnested AS (
+  SELECT 
+    (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'eventCategory') AS eventCategory,
+    (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'eventAction') AS eventAction,
+    (SELECT (value.string_value) FROM UNNEST(event_params) WHERE key = 'eventLabel') AS eventLabel
+  FROM 
+    `lifebuddi-ver2.analytics_181913311.events_*` AS t
+  WHERE 
+    _TABLE_SUFFIX BETWEEN '20180701' AND '20180906'
+    AND event_name = 'event'
+  LIMIT 1000
+)
+
+SELECT 
+  eventCategory
+  , eventAction
+  , ARRAY_TO_STRING(ARRAY_AGG(DISTINCT(IFNULL(eventLabel, ""))), "\r\n") AS eventLabels
+  , COUNT(eventCategory) AS count
+FROM event_params_unnested
+GROUP BY eventCategory, eventAction
